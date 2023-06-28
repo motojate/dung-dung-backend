b96a8b96d5223f9df21c2940d942909f070cbd6a
motojate <motojate@naver.com>
2023-06-28 23:59:05 +0900

    * 스케줄 생성

--- src/member/schedule/schedule.service.ts
+++ src/member/schedule/schedule.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class ScheduleService {}
+export class ScheduleService {
+  constructor(private prisma: PrismaService) {}
+
+  async createSchedule() {
+    return this.prisma.schedule.create({
+      data: {
+        startTime: new Date(),
+        endTime: new Date(),
+        title: 'test',
+        day: new Date()
+      }
+    })
+  }
+}
