d390d5090bfe18681c95f78d5539b9fda7df20a8
motojate <motojate@naver.com>
2023-07-04 23:54:35 +0900

    * 상속할 수 있게 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class AppService {
-  constructor() {
-    //
-  }
-
-  create(): void
-  update(): void
-  findAll(): any[]
-  findUnique(): any
+export abstract class AppService {
+  abstract create(): void
+  abstract update(): void
+  abstract findAll(): any[]
+  abstract findUnique(): any
+  abstract delete(): void
 }
