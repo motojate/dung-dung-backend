99d64ba2e2210874d18cd08b4e7f2b0b7d2b0e41
motojate <motojate@naver.com>
2023-07-08 23:04:09 +0900

    * 인풋 타입 추가

--- src/shared/interfaces/factory.interface.ts
+++ src/shared/interfaces/factory.interface.ts
@@ -1,7 +1,26 @@
-export interface BasedFactoryInterface<T> {
-  create(dto: any): Promise<boolean>
-  delete(dto: any): Promise<boolean>
-  update(dto: any): Promise<boolean>
+export interface BasedFactoryInterface<T, D> {
+  create(dto: CreateInput<D>): Promise<boolean>
+  delete(dto: DeleteInput<D>): Promise<boolean>
+  update(dto: UpdateInput<D>): Promise<boolean>
   findAll(): Promise<T[]>
-  findUnique(dto: any): Promise<T>
+  findUnique(dto: FindUniqueInput<D>): Promise<T>
+}
+
+interface CreateInput<T> {
+  data: T
+}
+
+interface DeleteInput<T> {
+  id: number
+  // 추가적인 필드들
+}
+
+interface UpdateInput<T> {
+  id: number
+  data: Partial<T>
+}
+
+interface FindUniqueInput<T> {
+  id: number
+  // 추가적인 필드들
 }
