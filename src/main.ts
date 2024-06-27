c1ac0668c6523cf4028d106334f4c051da6c099b
motojate <motojate@naver.com>
2024-06-28 01:49:05 +0900

    [FEAT] cors 설정

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true
+  });
   const port = 3200;
   await app.listen(port).then(() => {
     console.log(`Service listening : ${port}`);
