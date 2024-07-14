f44aa3e3ae69b885431a6c7eef6c67cf66530f3a
motojate <motojate@naver.com>
2024-07-14 22:51:16 +0900

    [FEAT] 오늘의 할 일 삭제하는 api 작성

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query, Res } from '@nestjs/common';
 import { CreateTodoDto } from './dtos/create-todo.dto';
 import { TodoService } from './todo.service';
 
@@ -18,5 +18,10 @@ export class TodoController {
     const userSeq = 'adb2ef0c-5468-41d1-9128-032c62a0358a'; // TODO: 토큰 검사 필요함
     const result = await this.todoService.getTodayTodo(userSeq);
     return result;
+  }
+
+  @Delete(':id')
+  async deleteTodo(@Query('id') id: number) {
+    await this.todoService.deleteTodo(id);
   }
 }
