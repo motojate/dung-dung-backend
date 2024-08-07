02bcfc1ce5138b5b8fe10c5467895a134525178e
motojate <motojate@naver.com>
2024-08-07 23:14:17 +0900

    [FEAT] 이력 지원 현황 관련 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { TodoModule } from './todo/todo.module';
 import { QuestionModule } from './question/question.module';
+import { JobApplicationModule } from './job-application/job-application.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule, QuestionModule],
+  imports: [PrismaModule, TodoModule, QuestionModule, JobApplicationModule],
   providers: []
 })
 export class AppModule {}
