123070195420cc6b4abe6b3ba6aea03c1183fe2a
motojate <motojate@naver.com>
2024-08-07 23:21:36 +0900

    [FEAT] 이력 지원 현황 관련 서비스 및 컨트롤러 추가

--- src/job-application/job-application.module.ts
+++ src/job-application/job-application.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { JobApplicationService } from './job-application.service';
+import { JobApplicationController } from './job-application.controller';
 
-@Module({})
+@Module({
+  providers: [JobApplicationService],
+  controllers: [JobApplicationController]
+})
 export class JobApplicationModule {}
